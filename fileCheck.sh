242e9e2f62683ae11d7298228f5cba53df79ad50
Faraz Behrouzieh <faraz.behrouzieh@stud.uni-bamberg.de>
2022-11-02 16:43:47 +0100

    Script Added.

---
+++ fileCheck.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#Author = Faraz Behrouzieh
+# This script gets a file as input and determines if it is a regular file or a directory.
+THEFILE=$1
+
+
+if [ -d $THEFILE ]
+   then
+     echo “This is a directory.”
+
+elif [ -b $THEFILE ]
+   then
+     echo “This is a 	Block special file.”
+
+elif [ -c $THEFILE ]
+   then
+     echo “This is a 	Character special file.”
+
+elif [ -s $THEFILE ]
+   then
+     echo “This is a 	Socket file.”
+
+elif [ -c $THEFILE ]
+   then
+     echo “This is a Symbol link”
+
+elif [ -f $THEFILE ]
+   then
+     echo “This is a regular file”
+
+fi
+
+
+#END
